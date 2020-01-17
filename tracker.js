9a0e7ca0310b46681692beb2c1dd3b7b670a9eff
Kacie Hatley <kaciehatley@gmail.com>
2020-01-17 12:29:36 -0500

    when view all employees is selected, displays employees and returns to initial question.

--- tracker.js
+++ tracker.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+const cTable = require('console.table');
 
 // create the connection information for the sql database
 var connection = mysql.createConnection({
@@ -20,15 +21,34 @@ var connection = mysql.createConnection({
   connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
-    inquirer
-    .prompt({
-        name: "initialChoice",
-        type: "list",
-        message: "What would you like to do??",
-        choices: ["View All Employees", "View All Employees By Department", "View All Employees By Manager", "Add Employee", "Remove Employee", "Update Employee Role, Update Employee Manager"]
-    })
-    .then(function(answer) {
-        console.log(answer);
-        connection.end();
-    })
+    start();
   });
+
+  function start() {
+      initialQuestion()
+  }
+
+  function initialQuestion() {
+    inquirer
+        .prompt({
+            name: "initialChoice",
+            type: "list",
+            message: "What would you like to do??",
+            choices: ["View All Employees", "View All Employees By Department", "View All Employees By Manager", "Add Employee", "Remove Employee", "Update Employee Role, Update Employee Manager"]
+        })
+        .then(function(answer) {
+            if (answer.initialChoice==="View All Employees") {
+                viewEmployees(answer);
+            }
+            connection.end();
+        })
+};
+
+function viewEmployees(answer) {
+    connection.query("SELECT * FROM employee", function(err, res) {
+        if (err) throw err;
+        // Log all results of the SELECT statement
+        console.log(cTable.getTable(res));
+        initialQuestion();
+    })
+}
